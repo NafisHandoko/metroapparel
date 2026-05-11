9982558dc2e068db10a9840d4e93045ef6bbdc44
Nafis Handoko <naffisioner@gmail.com>
2026-05-11 12:18:27 +0700

    fix docker entrypoint medusa script

--- docker/production/docker-entrypoint-medusa.sh
+++ docker/production/docker-entrypoint-medusa.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -e
+
 cd /server/apps/backend
 echo "Running database migrations..."
 pnpm exec medusa db:migrate
-echo "Starting Medusa (production)..."
+
+echo "Starting Medusa (production build)..."
+cd /server/apps/backend/.medusa/server
 exec pnpm start
